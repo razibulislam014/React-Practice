0d994189afaa31f3a20fac1814a50b3a04b56e99
razibulislam014 <razibulislam665@gmail.com>
2026-03-28 21:00:25 +0600

    removing from array

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import { useState } from "react";
 
-let nextId = 0;
+let initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
 export default function List() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState(initialArtists);
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          // artists.push({
-          //   id: nextId++,
-          //   name: name,
-          // });
-          setArtists([...artists, { id: nextId++, name: name }]); // Correct
-        }}
-      >
-        Add
-      </button>
       <ul>
         {artists.map((artist) => (
-          <li key={artist.id}>{artist.name}</li>
+          <li key={artist.id}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                setArtists(artists.filter((a) => a.id !== artist.id)); // Uses filter becouse making a new array instead of main array
+              }}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
